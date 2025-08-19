1adc56820a62d5c37d63c97c206f259efc3119e8
Taisiya Gorodnicheva <taisiya-1992@nail.ru>
2025-08-19 21:08:18 +0300

    update

---
+++ README.md
@@ -0,0 +1,16 @@
+# Интернет-магазин (Django)
+Учебный проект по созданию интернет-магазина на Django.
+
+## Стек:
+- Python 3.10+
+- Django 5+
+- Bootstrap 5
+
+## Установка:
+```bash
+git clone <repo_url>
+cd shop_project
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py runserver
---
+++ catalog/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
