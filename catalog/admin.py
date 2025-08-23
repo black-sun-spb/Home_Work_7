92c1cb725324eec7a621d6cf4802762d4931c5b1
Taisiya Gorodnicheva <taisiya-1992@nail.ru>
2025-08-24 02:43:39 +0300

    Add Category and Product models with migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_at', 'updated_at')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
