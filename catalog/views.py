57adb59a094b98545b37ed12032af12d625b4ffb
Taisiya Gorodnicheva <taisiya-1992@nail.ru>
2025-09-11 21:50:52 +0300

    еализован CRUD для продуктов с формами, валидация и стилизация

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
-from django.urls import reverse_lazy
+from django.views.generic import (
+    TemplateView, ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy, reverse
 from .models import Product, Contact
 from .forms import ProductForm
 
@@ -11,6 +14,7 @@ class HomeView(ListView):
     paginate_by = 6
     ordering = ['-created_at']
 
+
 # Страница контактов
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
@@ -20,15 +24,35 @@ class ContactsView(TemplateView):
         context["contacts"] = Contact.objects.all()
         return context
 
+
 # Детали товара
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
 
+
 # Добавление товара
 class ProductCreateView(CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/add_product.html'
+    success_url = reverse_lazy('catalog:home')
+
+
+# Редактирование товара
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/edit_product.html'
+
+    def get_success_url(self):
+        # После успешного редактирования перенаправляем на страницу товара
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
+
+
+# Удаление товара
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/delete_product.html'
     success_url = reverse_lazy('catalog:home')
